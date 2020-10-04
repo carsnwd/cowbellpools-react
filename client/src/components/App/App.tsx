8df72872826a1074047b2df95404e3ad84e3da3c
carsn <carsnwd@gmail.com>
2020-10-04 12:12:06 -0400

    Sidebar menu updates

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "./App.sass";
+import "./App.scss";
 import { Navbar } from "../Navbar/Navbar";
-import { Column, Columns } from "trunx";
+import { Column, Columns, Content, Footer } from "trunx";
 import SideMenu from "../SideMenu/SideMenu";
 
 function App() {
   return (
-    <div className="App" role="app">
+    <div className="app" role="app">
       <Navbar />
       <Columns isGapless>
-        <Column isOneQuarter>
+        <Column isOneFifthDesktop>
           <SideMenu></SideMenu>
         </Column>
         <Column>OK</Column>
       </Columns>
+      <Footer>
+        <Content>
+          <p>
+            <strong>Trunx</strong> by{" "}
+            <a href="http://g14n.info">Gianluca Casati</a>. The source code is
+            licensed <a href="http://g14n.info/mit-license">MIT</a>.
+          </p>
+        </Content>
+      </Footer>
     </div>
   );
 }
--- client/src/components/SideMenu/SideMenu.tsx
+++ client/src/components/SideMenu/SideMenu.tsx
@@ -1,5 +1,7 @@
+import { faFootballBall, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { Component } from "react";
-import { Li, Menu } from "trunx";
+import { Menu, Section } from "trunx";
 import "./SideMenu.scss";
 
 export default class SideMenu extends Component {
@@ -7,11 +9,35 @@ export default class SideMenu extends Component {
     return (
       <div className="side-menu">
         <Menu>
+          <Menu.Label>
+            <FontAwesomeIcon icon={faFootballBall} /> Picks
+          </Menu.Label>
           <Menu.List>
-            <Li>Foo</Li>
-            <Li>Bar</Li>
+            <li>
+              <a>Make Picks</a>
+            </li>
+            <li>
+              <a>View Picks</a>
+            </li>
+          </Menu.List>
+          <Menu.Label>
+            <FontAwesomeIcon icon={faUsers} /> Groups
+          </Menu.Label>
+          <Menu.List>
+            <li>
+              <a>My Groups</a>
+            </li>
+            <li>
+              <a>Create Group</a>
+            </li>
+            <li>
+              <a>Join Group</a>
+            </li>
           </Menu.List>
         </Menu>
+        <Section>
+          <div className="a-block"></div>
+        </Section>
       </div>
     );
   }
